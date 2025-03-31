c52ad7a16d5a089bd92f9d5da8f336e91e829dad
flobby41 <florian.cheheb@gmail.com>
2025-03-31 16:23:28 +0200

    vercel-dep6

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -12,14 +12,14 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   }));
 
   const collectionRoutes = collections.map((collection) => ({
-    url: `${process.env.NEXT_PUBLIC_SITE_URL}/search/${collection.handle}`,
+    url: `${process.env.NEXT_PUBLIC_SITE_URL}/search/${collection.id}`,
     lastModified: new Date(),
     changeFrequency: 'daily' as const,
     priority: 0.5,
   }));
 
   const productRoutes = products.map((product) => ({
-    url: `${process.env.NEXT_PUBLIC_SITE_URL}/product/${product.handle}`,
+    url: `${process.env.NEXT_PUBLIC_SITE_URL}/product/${product.id}`,
     lastModified: new Date(),
     changeFrequency: 'daily' as const,
     priority: 0.7,
