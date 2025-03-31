ff94d68bca06b1e1a5c766fed99af1d89f9ee9dc
flobby41 <florian.cheheb@gmail.com>
2025-03-31 16:16:42 +0200

    vercel-dep4

--- app/opengraph-image.tsx
+++ app/opengraph-image.tsx
@@ -1,5 +1,5 @@
 import OpengraphImage from 'components/opengraph-image';
 
 export default async function Image() {
-  return await OpengraphImage();
+  return await OpengraphImage({ title: 'Vercel Store' });
 }
