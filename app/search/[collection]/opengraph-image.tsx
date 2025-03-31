f5baab646fdb1590dd0e93ea39d78adfed313a29
flobby41 <florian.cheheb@gmail.com>
2025-03-31 16:19:51 +0200

    vercel-dep5

--- app/search/[collection]/opengraph-image.tsx
+++ app/search/[collection]/opengraph-image.tsx
@@ -1,46 +1,7 @@
-import { ImageResponse } from 'next/server';
-import { DEFAULT_OG_IMAGE } from 'lib/constants';
+import OpengraphImage from 'components/opengraph-image';
 
 export const runtime = 'edge';
-export const alt = 'Vercel Store Clone';
-export const size = 1200;
 
 export default async function Image({ params }: { params: { collection: string } }) {
-  return new ImageResponse(
-    (
-      <div
-        style={{
-          background: 'white',
-          width: '100%',
-          height: '100%',
-          display: 'flex',
-          alignItems: 'center',
-          justifyContent: 'center',
-          flexDirection: 'column',
-        }}
-      >
-        <img src={DEFAULT_OG_IMAGE} alt={alt} width={size} height={size} />
-        <div
-          style={{
-            position: 'absolute',
-            bottom: 0,
-            left: 0,
-            right: 0,
-            padding: '20px',
-            background: 'rgba(0,0,0,0.7)',
-            color: 'white',
-            textAlign: 'center',
-            fontSize: '48px',
-            fontWeight: 'bold',
-          }}
-        >
-          {params.collection}
-        </div>
-      </div>
-    ),
-    {
-      width: size,
-      height: size,
-    }
-  );
+  return await OpengraphImage({ title: params.collection });
 }
